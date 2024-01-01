8ca7fc1a3ea8e667b2d83293618a409a9157abb6
ricardochust <ricardochustfides@gmail.com>
2024-01-01 20:24:18 +0100

    removed compilation problem with no eth

--- LCU-SLAVE/Core/Inc/main.h
+++ LCU-SLAVE/Core/Inc/main.h
@@ -59,6 +59,15 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define MCO_Pin GPIO_PIN_0
+#define MCO_GPIO_Port GPIOH
+#define RMII_MDC_Pin GPIO_PIN_1
+#define RMII_MDC_GPIO_Port GPIOC
+#define RMII_REF_CLK_Pin GPIO_PIN_1
+#define RMII_REF_CLK_GPIO_Port GPIOA
+#define RMII_MDIO_Pin GPIO_PIN_2
+#define RMII_MDIO_GPIO_Port GPIOA
+#define RMII_CRS_DV_Pin GPIO_PIN_7
 #define PWM_OUT_Pin GPIO_PIN_5
 #define PWM_OUT_GPIO_Port GPIOE
 #define PWM_OUTE6_Pin GPIO_PIN_6
@@ -93,6 +102,11 @@ void Error_Handler(void);
 #define ADC_16BITA5_GPIO_Port GPIOA
 #define ADC_16BITA6_Pin GPIO_PIN_6
 #define ADC_16BITA6_GPIO_Port GPIOA
+#define RMII_CRS_DV_GPIO_Port GPIOA
+#define RMII_RXD0_Pin GPIO_PIN_4
+#define RMII_RXD0_GPIO_Port GPIOC
+#define RMII_RXD1_Pin GPIO_PIN_5
+#define RMII_RXD1_GPIO_Port GPIOC
 #define ADC_16BITB0_Pin GPIO_PIN_0
 #define ADC_16BITB0_GPIO_Port GPIOB
 #define ADC_16BITB1_Pin GPIO_PIN_1
@@ -119,6 +133,8 @@ void Error_Handler(void);
 #define PWM_OUTE13_GPIO_Port GPIOE
 #define PWM_OUTE14_Pin GPIO_PIN_14
 #define PWM_OUTE14_GPIO_Port GPIOE
+#define RMII_TXD1_Pin GPIO_PIN_13
+#define RMII_TXD1_GPIO_Port GPIOB
 #define PWM_OUTB14_Pin GPIO_PIN_14
 #define PWM_OUTB14_GPIO_Port GPIOB
 #define PWM_OUTB15_Pin GPIO_PIN_15
@@ -129,12 +145,20 @@ void Error_Handler(void);
 #define PWM_OUTD13_GPIO_Port GPIOD
 #define PWM_OUTD15_Pin GPIO_PIN_15
 #define PWM_OUTD15_GPIO_Port GPIOD
+#define ENCODER_1_Pin GPIO_PIN_6
+#define ENCODER_1_GPIO_Port GPIOC
+#define ENCODER_1C7_Pin GPIO_PIN_7
+#define ENCODER_1C7_GPIO_Port GPIOC
 #define PWM_OUTC8_Pin GPIO_PIN_8
 #define PWM_OUTC8_GPIO_Port GPIOC
 #define PWM_IN_Pin GPIO_PIN_9
 #define PWM_IN_GPIO_Port GPIOC
 #define SPI3_SS_Pin GPIO_PIN_3
 #define SPI3_SS_GPIO_Port GPIOD
+#define RMII_TX_EN_Pin GPIO_PIN_11
+#define RMII_TX_EN_GPIO_Port GPIOG
+#define RMII_TXD0_Pin GPIO_PIN_13
+#define RMII_TXD0_GPIO_Port GPIOG
 #define PWM_OUTB4_Pin GPIO_PIN_4
 #define PWM_OUTB4_GPIO_Port GPIOB
 #define PWM_OUTB5_Pin GPIO_PIN_5
@@ -147,6 +171,12 @@ void Error_Handler(void);
 #define PWM_OUTB8_GPIO_Port GPIOB
 #define PWM_OUTB9_Pin GPIO_PIN_9
 #define PWM_OUTB9_GPIO_Port GPIOB
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
