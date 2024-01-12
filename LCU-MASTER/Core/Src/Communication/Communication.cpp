671ec8b6b81d88c12249eb3867b45a7c3e048699
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-01-12 10:17:44 +0100

    add check for communication received

--- LCU-MASTER/Core/Src/Communication/Communication.cpp
+++ LCU-MASTER/Core/Src/Communication/Communication.cpp
@@ -5,6 +5,7 @@ uint8_t Communication::spi_id = 0;
 SPIPacket* Communication::test_pwm_packets[TEST_PWM_PACKET_AMOUNT]{nullptr};
 ServerSocket* Communication::gui_connection = nullptr;
 DatagramSocket* Communication::udp_connection = nullptr;
-uint8_t Communication::pwm_to_change = 0;
+uint16_t Communication::pwm_to_change = 0;
 float Communication::duty_to_change = 0;
+DigitalOutput* Communication::test_order_received = nullptr;
 
