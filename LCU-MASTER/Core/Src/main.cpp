6d0ff0f3547d67233257b4d2d7dab5b5f5561eb0
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-01-30 20:20:13 +0100

    added op_led to sync with RST signal

--- LCU-MASTER/Core/Src/main.cpp
+++ LCU-MASTER/Core/Src/main.cpp
@@ -11,12 +11,13 @@ int main(void)
 	static_assert(HSE_VALUE==25'000'000,"INVALID HSE value for BOARD");
 	Communication::test_order_received = new DigitalOutput(PG4);
 	DigitalOutput reset_5{PB14};
+	DigitalOutput operational_RST{PG8};
 	uint8_t fault_10 = DigitalInput::inscribe(PE11);
 	uint8_t ready_10 = DigitalInput::inscribe(PE10);
 
 
 	LCU lcu_master;
-
+	operational_RST.turn_off();
 	reset_5.turn_off();
 	Communication::test_order_received->turn_on();
 	uint8_t unused_variable = Time::set_timeout(3000,[&](){
@@ -29,7 +30,9 @@ int main(void)
 	while(1) {
 		if(DigitalInput::read_pin_state(ready_10) == PinState::ON && DigitalInput::read_pin_state(fault_10) == PinState::ON){
 			reset_5.turn_on();
+			operational_RST.turn_on();
 		}else{
+			operational_RST.turn_off();
 			reset_5.turn_off();
 		}
 
