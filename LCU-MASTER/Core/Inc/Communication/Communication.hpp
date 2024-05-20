014c0fb0331a4f31854540ebe698e30ea3818822
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-05-20 09:45:23 +0200

    update to developemt

--- LCU-MASTER/Core/Inc/Communication/Communication.hpp
+++ LCU-MASTER/Core/Inc/Communication/Communication.hpp
@@ -101,7 +101,7 @@ public:
 	//###################  PERIODIC FUNCTIONS  #########################
 
 	static void send_lcu_data_to_backend(){
-		udp_connection->send(*EthernetPackets[SEND_LPU_TEMPERATURES_TCP_PACKET_INDEX]);
+		udp_connection->send_packet(*EthernetPackets[SEND_LPU_TEMPERATURES_TCP_PACKET_INDEX]);
 	}
 
 	static void lcu_data_transaction(){
